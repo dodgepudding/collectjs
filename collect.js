1917c8f924613df3352985aa283a9105bf6aa759
Administrator <Administrator@SDWM-20130331FJ.lan>
2013-05-22 01:22:05 +0800

    show dialog

--- collect.js
+++ collect.js
@@ -1,6 +1,9 @@
 (function(){
 	var tagprefix = 'demo_';
-	var shareurl = 'http://www.domain.com/share.php?url=';
+	var shareurl = 'http://www.baidu.com?url=';
+	var dialogtitle = 'tips';
+	var dialogwidth = '720';
+	var dialogheight = '600';
 	var dialogmask;
 	var dialoghandle;
 	function createElement(e, t) {
@@ -10,46 +13,52 @@
 	function getElement(e) {
 		return document.getElementById(tagprefix + e);
 	}
-	collectjs_closedialog = function(){
+	var closeDialog = function(){
 		var astyle = getElement("style");
 		var p = astyle.parentNode;
-		p.removeChild(dialogmask), p.removeChild(dialoghandle);p.removeChild(astyle);
+		p.removeChild(dialogmask);
+		p.removeChild(dialoghandle);
+		p.removeChild(astyle);
 		return;
 	};
-	var validateurl = function(url) {
+	var validateUrl = function(url) {
         if (url.indexOf('http://')!==0 && url.indexOf('https://')!==0) return false;
         //TODO: validate url
         return true;
-    }
+    };
 	var fetch = function(){
 		var url = location.href;
-		if (!validateurl(url)) {
+		if (!validateUrl(url)) {
 			alert('url error');
 			return;
 		}
 		var styledom = getElement("style");
 		var bm = 0 <= navigator.userAgent.indexOf("MSIE") && 0 > navigator.userAgent.indexOf("Opera");
 		if (!styledom || "link" !== styledom.tagName.toLowerCase()) {
-			styledom = createElement("style","style");
+			styledom = createElement("style","link");
 			styledom.rel="stylesheet";
 			styledom.type = "text/css"; 
-			styledom.href = "/css/baobei.css"; 
+			styledom.className= "collectjs";
+			styledom.href = "css/collect.css"; 
 			bm ? document.getElementsByTagName("head")[0].appendChild(styledom): document.body.appendChild(styledom);
 			dialogmask = createElement("mask");
+			dialogmask.className = tagprefix+"mask";
 			document.body.appendChild(dialogmask);
 			dialoghandle = createElement("sharebox");
+			dialoghandle.className = tagprefix+'outer '+tagprefix+'state-lock '+tagprefix+'state-visible '+tagprefix+'state-focus';
 			document.body.appendChild(dialoghandle);
 			var tourl = shareurl+encodeURIComponent(url);
-			var content = '<div class="d-outer d-state-lock d-state-visible d-state-focus"><table class="d-border"><tbody><tr><td class="d-nw"></td><td class="d-n"></td><td class="d-ne"></td></tr><tr><td class="d-w"></td><td class="d-c"><div class="d-inner"><table class="d-dialog"><tbody><tr><td class="d-header"><div class="d-titleBar"><div class="d-title" style="">米铺采集</div><a class="d-close" href="javascript:collectjs_closedialog();">×</a></div></td></tr><tr><td class="d-main" style="width: 600px; height: 700px; "><div class="d-content"><iframe width="600px" height="700px" name="idialogform" id="idialogform" frameborder="0" allowtransparency="true" marginheight="0" marginwidth="0" border="0" src="'+tourl+'"></iframe></div></td></tr><tr><td class="d-footer"></td></tr></tbody></table></div></td><td class="d-e"></td></tr><tr><td class="d-sw"></td><td class="d-s"></td><td class="d-se"></td></tr></tbody></table></div>';
+			var content = '<table class="'+tagprefix+'border"><tbody><tr><td class="'+tagprefix+'nw"></td><td class="'+tagprefix+'n"></td><td class="'+tagprefix+'ne"></td></tr><tr><td class="'+tagprefix+'w"></td><td class="'+tagprefix+'c"><div class="'+tagprefix+'inner"><table class="'+tagprefix+'dialog"><tbody><tr><td class="'+tagprefix+'header"><div class="'+tagprefix+'titleBar"><div class="'+tagprefix+'title" style="">tips</div><a class="'+tagprefix+'close" href="javascript:document.'+tagprefix+'.closeDialog()">×</a></div></td></tr><tr><td class="'+tagprefix+'main" style="width: '+dialogwidth+'px; height: '+dialogheight+'px; "><div class="'+tagprefix+'content"><iframe width="'+dialogwidth+'px" height="'+dialogheight+'px" name="idialogform" id="idialogform" frameborder="0" allowtransparency="true" marginheight="0" marginwidth="0" border="0" src="'+tourl+'"></iframe></div></td></tr><tr><td class="'+tagprefix+'footer"></td></tr></tbody></table></div></td><td class="'+tagprefix+'e"></td></tr><tr><td class="'+tagprefix+'sw"></td><td class="'+tagprefix+'s"></td><td class="'+tagprefix+'se"></td></tr></tbody></table>';
 			dialoghandle.innerHTML = content;
 		}
 	};
-	if (typeof document[domainname]=='undefined' || !document[domainname]._loaded) {
-		document[domainname] = document[domainname] || {};
-		document[domainname]._loaded = !0; 
-		document[domainname].fetchShare = fetch;
-		document[domainname].fetchShare();
+	if (typeof document[tagprefix]=='undefined' || !document[tagprefix]._loaded) {
+		document[tagprefix] = document[tagprefix] || {};
+		document[tagprefix]._loaded = !0; 
+		document[tagprefix].fetchShare = fetch;
+		document[tagprefix].closeDialog = closeDialog;
+		document[tagprefix].fetchShare();
 	} else {
-		document[domainname].fetchShare();
+		document[tagprefix].fetchShare();
 	}
 })();
