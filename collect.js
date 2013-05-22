fe126ca485f61e2e521a108eee1d4b8817615373
dodgepudding <dodgepudding@gmail.com>
2013-05-22 15:12:55 +0000

    change dialog style to bootstrap dialog

--- collect.js
+++ collect.js
@@ -1,7 +1,8 @@
 (function(){
 	var tagprefix = 'demo_';
 	var shareurl = 'http://www.baidu.com?url=';
-	var dialogtitle = 'tips';
+	var stylecss = 'http://127.0.0.1/~admin/collectjs/css/collect.css';
+	var dialogtitle = 'Share';
 	var dialogwidth = '720';
 	var dialogheight = '600';
 	var dialogmask;
@@ -16,10 +17,14 @@
 	var closeDialog = function(){
 		var astyle = getElement("style");
 		var p = astyle.parentNode;
-		p.removeChild(dialogmask);
-		p.removeChild(dialoghandle);
-		p.removeChild(astyle);
-		return;
+		var dialogmask = document.getElementById(tagprefix + "mask");
+		var dialoghandle = document.getElementById(tagprefix + "sharebox");
+		dialoghandle.style.display="none";
+		dialogmask.className = tagprefix+'mask hide';
+		//p.removeChild(dialogmask);
+		//p.removeChild(dialoghandle);
+		//p.removeChild(astyle);
+		return false;
 	};
 	var validateUrl = function(url) {
         if (url.indexOf('http://')!==0 && url.indexOf('https://')!==0) return false;
@@ -33,24 +38,32 @@
 			return;
 		}
 		var styledom = getElement("style");
-		var bm = 0 <= navigator.userAgent.indexOf("MSIE") && 0 > navigator.userAgent.indexOf("Opera");
 		if (!styledom || "link" !== styledom.tagName.toLowerCase()) {
+			var bm = 0 <= navigator.userAgent.indexOf("MSIE") && 0 > navigator.userAgent.indexOf("Opera");
 			styledom = createElement("style","link");
 			styledom.rel="stylesheet";
 			styledom.type = "text/css"; 
 			styledom.className= "collectjs";
-			styledom.href = "css/collect.css"; 
+			styledom.href = stylecss; 
 			bm ? document.getElementsByTagName("head")[0].appendChild(styledom): document.body.appendChild(styledom);
-			dialogmask = createElement("mask");
-			dialogmask.className = tagprefix+"mask";
-			document.body.appendChild(dialogmask);
-			dialoghandle = createElement("sharebox");
-			dialoghandle.className = tagprefix+'outer '+tagprefix+'state-lock '+tagprefix+'state-visible '+tagprefix+'state-focus';
-			document.body.appendChild(dialoghandle);
-			var tourl = shareurl+encodeURIComponent(url);
-			var content = '<table class="'+tagprefix+'border"><tbody><tr><td class="'+tagprefix+'nw"></td><td class="'+tagprefix+'n"></td><td class="'+tagprefix+'ne"></td></tr><tr><td class="'+tagprefix+'w"></td><td class="'+tagprefix+'c"><div class="'+tagprefix+'inner"><table class="'+tagprefix+'dialog"><tbody><tr><td class="'+tagprefix+'header"><div class="'+tagprefix+'titleBar"><div class="'+tagprefix+'title" style="">tips</div><a class="'+tagprefix+'close" href="javascript:document.'+tagprefix+'.closeDialog()">×</a></div></td></tr><tr><td class="'+tagprefix+'main" style="width: '+dialogwidth+'px; height: '+dialogheight+'px; "><div class="'+tagprefix+'content"><iframe width="'+dialogwidth+'px" height="'+dialogheight+'px" name="idialogform" id="idialogform" frameborder="0" allowtransparency="true" marginheight="0" marginwidth="0" border="0" src="'+tourl+'"></iframe></div></td></tr><tr><td class="'+tagprefix+'footer"></td></tr></tbody></table></div></td><td class="'+tagprefix+'e"></td></tr><tr><td class="'+tagprefix+'sw"></td><td class="'+tagprefix+'s"></td><td class="'+tagprefix+'se"></td></tr></tbody></table>';
-			dialoghandle.innerHTML = content;
 		}
+		dialogmask = getElement("mask")?getElement("mask"):createElement("mask");;
+		dialogmask.className = tagprefix+"mask";
+		dialogmask.onclick = closeDialog;
+		document.body.appendChild(dialogmask);
+		dialoghandle = getElement("sharebox")?getElement("sharebox"):createElement("sharebox");
+		dialoghandle.className = tagprefix+'modal fade';
+		dialoghandle.style.display="none";
+		var tourl = shareurl+encodeURIComponent(url);
+		var content = '<div class="modal-header"><button type="button" class="close" onclick="javascript:document.'+tagprefix+'.closeDialog()">&times;</button><h3>'+dialogtitle+'</h3></div><div class="modal-body"><iframe width="'+dialogwidth+'px" height="'+dialogheight+'px" id="'+tagprefix+'iframe" frameborder="0" allowtransparency="true" marginheight="0" marginwidth="0" border="0" scrolling="no" src="'+tourl+'"></iframe></div><div class="modal-footer"></div>';
+		dialoghandle.innerHTML = content;
+		document.body.appendChild(dialoghandle);
+		setTimeout(function(){
+			dialoghandle.style.display="block";
+			setTimeout(function(){
+				dialoghandle.className = tagprefix+'modal fade in';
+			},200);
+		},100);
 	};
 	if (typeof document[tagprefix]=='undefined' || !document[tagprefix]._loaded) {
 		document[tagprefix] = document[tagprefix] || {};
